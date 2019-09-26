c2866621bc0109a0af9055076ff156539df530f2
Daniel ILse <dantilse@gmail.com>
2019-09-25 21:12:38 -0500

    Add styles for header using CSS Modules

--- src/components/header.js
+++ src/components/header.js
@@ -2,48 +2,22 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-import "../scss/components/_header.scss"
+import headerStyles from "../scss/components/header.module.scss"
+
+let hs = headerStyles
 
 const Header = ({ menuLinks, siteTitle }) => (
-  <header className="site-header">
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-      <div>
-        <nav>
-          <ul style={{ display: "flex", flex: 1 }}>
-            {menuLinks.map(link => (
-              <li
-                key={link.name}
-                style={{
-                  listStyleType: `none`,
-                  padding: `1rem`,
-                }}
-              >
-                <Link style={{ color: `white` }} to={link.link}>
-                  {link.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
-    </div>
+  <header className={hs.siteHeader}>
+    <h1 class={hs.siteTitle}>
+      <Link to="/">{siteTitle}</Link>
+    </h1>
+    <ul class={hs.siteNav}>
+      {menuLinks.map(link => (
+        <li key={link.name}>
+          <Link to={link.link}>{link.name}</Link>
+        </li>
+      ))}
+    </ul>
   </header>
 )
 
