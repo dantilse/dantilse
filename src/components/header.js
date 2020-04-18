8410b5235e865a5c8033e28feead616ca9583dc6
Daniel Ilse <daniel.ilse@nxp.com>
2020-04-17 22:30:04 -0500

    Add simple mobile menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,38 +1,52 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useState } from "react"
 import LayoutContainer from "./layout-container"
 
 import headerStyles from "../scss/components/header.module.scss"
 
 let hs = headerStyles
 
-const Header = ({ menuLinks, siteTitle, siteDescription }) => (
-  <header className={hs.siteHeader}>
-    <LayoutContainer>
-      <div className={hs.siteHeaderInner}>
-        <Link className={hs.siteTitle} to="/">
-          <span className={hs.siteName}>{siteTitle}</span>
-          <span className={hs.siteDescription}>{siteDescription}</span>
-        </Link>
-        <ul class={hs.siteNav}>
-          {menuLinks.map(link => (
-            <li key={link.name}>
-              {link.link.startsWith("//") || link.link.startsWith("mailto:") ? (
-                <a href={link.link}>
-                  {/* target="_blank" rel="noreferrer noopener" */}
-                  {link.name}
-                </a>
-              ) : (
-                <Link to={link.link}>{link.name}</Link>
-              )}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </LayoutContainer>
-  </header>
-)
+const Header = ({ menuLinks, siteTitle, siteDescription }) => {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <header className={hs.siteHeader}>
+      <LayoutContainer>
+        <div className={hs.siteHeaderInner}>
+          <Link className={hs.siteTitle} to="/">
+            <span className={hs.siteName}>{siteTitle}</span>
+            <span className={hs.siteDescription}>{siteDescription}</span>
+          </Link>
+          <div className={hs.siteNav}>
+            <button
+              className={hs.siteNavToggle}
+              open={open}
+              onClick={() => setOpen(!open)}
+            >
+              &#9776;
+            </button>
+            <ul className={open ? hs.open : ""}>
+              {menuLinks.map(link => (
+                <li key={link.name}>
+                  {link.link.startsWith("//") ||
+                  link.link.startsWith("mailto:") ? (
+                    <a href={link.link}>
+                      {/* target="_blank" rel="noreferrer noopener" */}
+                      {link.name}
+                    </a>
+                  ) : (
+                    <Link to={link.link}>{link.name}</Link>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </LayoutContainer>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
